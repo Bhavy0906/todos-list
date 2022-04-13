a7fd7fe40b41f16857df5a5f463f8483c1060b36
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2022-04-14 01:28:31 +0530

    Create About.js

---
+++ src/Components/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div className='container my-3'>
+            <h3><strong>Hello Folks</strong>!</h3>
+            <p>
+                I am an undergraduate student persuing MSc. Mathematics and B.E. Computer Sciences at Birla Institute of Technology and Sciences, Pilani.
+            </p>
+            <p>
+                Get in touch with me on my social links.
+            </p>
+            <p>
+                <strong>Bhavy Goel</strong>
+            </p>
+        </div>
+    )
+}
