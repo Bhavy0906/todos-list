af43a4db70ca6e40512dbb9165d9de30465ca4ac
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2022-04-14 01:28:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header";
+import AddTodo from "./Components/AddTodo";
+import Todos from './Components/Todos'
+import About from './Components/About'
+import Footer from './Components/Footer'
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  let initTodos;
+  let initCompletedTodos;
+  let initCurrentTodos;
+  if (localStorage.getItem("currentTodos") === null) {
+    initCurrentTodos = [];
+  } else {
+    initCurrentTodos = JSON.parse(localStorage.getItem("currentTodos"));
+  }
+  if (localStorage.getItem("completedTodos") === null) {
+    initCompletedTodos = [];
+  } else {
+    initCompletedTodos = JSON.parse(localStorage.getItem("completedTodos"));
+  }
+  const onDelete = (todo) => {
+    setCompletedTodos(completedTodos.filter(e => e !== todo));
+    setCurrentTodos(currentTodos.filter(e => e !== todo));
+  }
+
+  const onComplete = (todo) => {
+    currentTodos.splice(currentTodos.indexOf(todo),1);
+    setCurrentTodos([...currentTodos]);
+    setCompletedTodos([...completedTodos, todo]);
+  }
+  
+  const onUndo = (todo) => {
+    completedTodos.splice(completedTodos.indexOf(todo),1);
+    setCurrentTodos([...currentTodos, todo]);
+    setCompletedTodos([...completedTodos]);
+  }
+
+  const addTodo = (title, desc) => {
+    const newTodo = {
+      sno: currentTodos.length + 1,
+      title: title,
+      desc: desc
+    }
+    setCurrentTodos([...currentTodos, newTodo]);
+  }
+
+  const [currentTodos, setCurrentTodos] = useState(initCurrentTodos);
+  const [completedTodos, setCompletedTodos] = useState(initCompletedTodos);
+  useEffect(() => {
+    localStorage.setItem("currentTodos", JSON.stringify(currentTodos))
+  }, [currentTodos])
+  useEffect(() => {
+    localStorage.setItem("completedTodos", JSON.stringify(completedTodos))
+  }, [completedTodos])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header title='Todos List' searchBar={false} />
+        <Routes>
+          <Route path='/' element = {
+                  <>
+                  <AddTodo addTodo={addTodo} />
+                  <Todos todos={[...currentTodos, ...completedTodos]} onDelete={onDelete} onComplete = {onComplete} onUndo = {onUndo} />
+                  </>
+          } />
+          <Route path='about' element = {<About />}/>
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
